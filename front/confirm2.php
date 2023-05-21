f819023f666f626c372a3e0a0810c075d6db36d9
Jupy9804 <130624772+Jupy9804@users.noreply.github.com>
2023-05-21 19:58:01 +0900

    Add files via upload

---
+++ front/confirm2.php
@@ -0,0 +1,217 @@
+<html lang="en"><head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densitydpi=medium-dpi">
+    <!--<title>SHERLOCK HOLMES - 셜록홈즈 방탈출 게임</title>-->
+    <title>Cube Escape</title>
+    <script language="javascript">
+    document.title="Cube Escape - 큐브형식의 방 탈출 게임";
+    </script>
+    	<script  src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+    <link rel="stylesheet" href="css/board.css" type="text/css">
+<link rel="stylesheet" href="css/board1.css" type="text/css">
+
+    
+        <meta name="keywords" content="신천 방탈출카페,신림 방탈출카페,홍대 방탈출카페,잠실 방탈출카페,강남 방탈출카페,방탈출카페">
+    <meta name="description" content="강남,홍대,잠실,노량진,신림 등 전국 40호점 국내최대 셜록 방탈출 카페">
+    <meta property="og:description" content="강남,홍대,잠실,노량진,신림 등 전국 40호점 국내최대 셜록 방탈출 카페">
+    <meta name="naver-site-verification" content="90315006b3ecb880974d00ad95d55b4518b7de7b">
+        
+    
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="현생탈출 방탈출 카페">
+ 
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+    </head>
+    <body>
+        <header id="header">
+            <div class="inner">
+                <div class="header_logo">
+                    <a href="/"><img src="https://raw.githubusercontent.com/dudxoor68/teamProject/main/front/img/logo.png"></a>
+                </div>
+                <nav class="header_nav">
+                    <ul class="depth1">
+                        <li class="theme">
+                            <a href="/theme">테마</a>
+                        </li>
+                       <li class="branch">
+                            <a href="/branch">지점소개</a>
+                        </li>
+                        <li class="board">
+                            <a href="/board/press.php">공지사항</a>
+                        </li>
+                        <li class="reservation active">
+                            <a href="/reservation">예약하기</a>
+                        </li>
+            </div>
+        </header>
+<div id="board" class="body">
+	<section id="title_area">
+		<div class="container">
+			<h1>예약조회</h1>
+			<h4>Reservation</h4>
+			<div class="line"></div>
+		</div>
+	</section>
+
+	<section class="board_list">
+		<div class="inner container">
+			<div id="board_tab">
+
+				<form method="get" action="/board/press.php" name="frm" id="frm">
+				</form>
+			</div>
+			<div id="board_list">
+				<table class="table">
+					<colgroup>
+						<col class="num" width="10%">
+						<col width="">
+						<col width="100px">
+						<col width="100px">
+						<col class="view" width="10%">
+                  
+					</colgroup>
+					<thead>
+						<tr class="first">
+							<th>예약자명</th>
+							<th>전화번호</th>
+							<th>예약날짜</th>
+                            <th>예약시간</th>
+                            <th>예약인원</th>
+							<th>지역</th>
+							<th>지점</th>
+                            <th>테마</th>
+                            <th>가격</th>
+                            <th>결제방식</th>
+						</tr>
+					</thead>
+					<tbody>
+
+						<tr>
+							<td class="name">정의표</td>
+							<td class="num">010-1111-2222</td>
+							<td class="date">2023-05-21</td>
+							<td class="time">12:30</td>
+							<td class="per">3명</td>
+                            <td class="region">경기도</td>
+                            <td class="point">신구대점</td>
+                            <td class="theme">공포</td>
+                            <td class="price">35,000</td>
+                            <td class="pay">카카오페이</td>
+						</tr>
+
+						<tr>
+							<td class="name"></td>
+							<td class="num"></td>
+							<td class="date"></td>
+							<td class="time"></td>
+							<td class="per"></td>
+                            <td class="region"></td>
+                            <td class="point"></td>
+                            <td class="theme"></td>
+                            <td class="price"></td>
+                            <td class="pay"></td>
+						</tr>
+						<tr>
+							<td class="name"></td>
+							<td class="num"></td>
+							<td class="date"></td>
+							<td class="time"></td>
+							<td class="per"></td>
+                            <td class="region"></td>
+                            <td class="point"></td>
+                            <td class="theme"></td>
+                            <td class="price"></td>
+                            <td class="pay"></td>
+						</tr>
+
+						<tr>
+							<td class="name"></td>
+							<td class="num"></td>
+							<td class="date"></td>
+							<td class="time"></td>
+							<td class="per"></td>
+                            <td class="region"></td>
+                            <td class="point"></td>
+                            <td class="theme"></td>
+                            <td class="price"></td>
+                            <td class="pay"></td>
+						</tr>
+
+						<tr>
+							<td class="name"></td>
+							<td class="num"></td>
+							<td class="date"></td>
+							<td class="time"></td>
+							<td class="per"></td>
+                            <td class="region"></td>
+                            <td class="point"></td>
+                            <td class="theme"></td>
+                            <td class="price"></td>
+                            <td class="pay"></td>
+						</tr>
+
+						
+					</tbody>
+				</table>
+			</div>
+	
+    </ul>				
+
+		</div>
+	</div>
+	</section>
+</div>
+
+<script type="text/javascript">
+	$(function(){
+		
+	});
+</script>
+<footer id="footer">
+		<section class="footer_top">
+			<div class="inner container">
+				<ul class="footer_list">
+	                <li><a href="/privacy/personal.php">개인정보취급방침</a></li>
+	                <li><a href="/privacy/agreement.php">이용약관</a></li>
+	                <li><a href="/branch">지점소개</a></li>
+	                <li><a href="http://unreal-company.co.kr">프랜차이즈 가맹문의</a></li>
+	            </ul>
+	            <button type="button" class="btn_top" style="visibility: hidden;">BACK TO TOP</button>
+			</div>
+		</section>
+		<section class="footer_bottom">
+			<div class="inner container">
+				<div class="site_info">
+					<a href="/" class="footer_logo"><img src="/images/common/footer_logo.png"></a>
+					<ul class="site_info_1">
+		                <li><span>상호명</span> 셜록홈즈</li>
+		                <li><span>주소</span> 경기도 안양시 동안구 시민대로327번길 11-41 언리얼컴퍼니</li>
+		            </ul>
+		            <ul class="site_info_2">
+		               <li><span>(주)언리얼컴퍼니</span> </li>
+					   <li><span> 사업자등록번호</span> 848-81-00487</li>
+					   <li><span> 통신판매업 신고 </span> 2022-안양동안-1103호 </li>
+					   <!-- 190926 요청에 따라 연락 이메일 교체 -->
+		                <li><span>대표전화</span> 1800.6777  &nbsp;&nbsp;  / &nbsp;&nbsp;  E-mail unrealcompany@naver.com<!--info@unrealcompany.co.kr--></li>
+		            </ul>
+		            <div class="copyright">Copyright ⓒ The Team Sherlock-Hormes. All rights reserved.</div>
+				</div>
+				<!-- <div class="bank_info">
+					<p>신한은행 / 예금주 : 권충도 </p>
+					<div class="acount_num">
+						<img src="/images/common/bank_logo.png">
+						1104470884800
+					</div>
+				</div> -->
+	         
+			</div>
+		</section>
+	</footer>
+
+</body></html>
